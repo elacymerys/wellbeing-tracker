89fee3ebfb39997038d12bfda76d14db58a9eef9
elacymerys <ela.cymerys@gmail.com>
2022-11-21 18:07:25 +0100

    Create Mood Logger page template

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,83 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+    IonCol,
+    IonContent,
+    IonGrid,
+    IonHeader, IonIcon,
+    IonPage,
+    IonRow,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import './Tab1.css';
+import { CalendarService } from "../services/calendar-service";
+import React from "react";
+import { calendarOutline } from "ionicons/icons";
+
+const Calendar: React.FC = () => {
+    const today = CalendarService.getCurrentDate();
+
+    const labels = CalendarService.getDays().map(day => {
+        return <IonCol>{ day[0] }</IonCol>;
+    });
+
+    const dates = CalendarService.getCurrentWeekDates().map(date => {
+        return <IonCol onClick={ e => console.log(date) } className={ date === today ? "today" : "" }>{ date }</IonCol>;
+    });
+
+    return (
+        <IonGrid className="weekly-calendar">
+            <IonRow>
+                <IonCol></IonCol>
+                <IonCol size="auto">
+                    { CalendarService.getCurrentMonth() }
+                </IonCol>
+                <IonCol size="auto">
+                    <IonIcon icon={ calendarOutline } size="small"></IonIcon>
+                </IonCol>
+            </IonRow>
+            <IonRow className="labels">
+                { labels }
+            </IonRow>
+            <IonRow className="dates">
+                { dates }
+            </IonRow>
+        </IonGrid>
+    );
+}
+
+const MoodLogger: React.FC = () => {
+    return (
+        <div className="circle">
+            <p className="header">Mood <br /> <span className="subheader">How do you feel?</span></p>
+            <div className="emoji">
+                😃
+            </div>
+            <button onClick={ e => console.log('Log your mood') }>
+                Log mood
+            </button>
+        </div>
+    );
+}
 
 const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Tab 1</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Tab 1</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <Calendar />
+                <MoodLogger />
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab1;
